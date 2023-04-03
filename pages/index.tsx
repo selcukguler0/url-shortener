4e5c73981d24ad21d9a44bb3f8b6024527e8b51e
Selcuk <selcuk.guler00@gmail.com>
2023-04-03 19:44:11 +0300

    added loading to shorting button

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
 	const [url, setUrl] = useState<string>("");
+	const [btnLoading, setBtnLoading] = useState<boolean>(false);
 
 	const shortUrl = async () => {
 		const res = await fetch("/api/shortUrl", {
@@ -87,12 +88,21 @@ export default function Home() {
 								/>
 							</div>
 							<div className="flex items-center justify-between pt-4">
-								<button
+								{btnLoading ? (
+									<button
 									onClick={shortUrl}
 									className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
 									type="button">
 									{"Let's Short!"}
-								</button>
+									</button>
+								) : (
+									<button
+									onClick={shortUrl}
+									className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+									type="button" disabled>
+									{"Shorting..."}
+									</button>
+								)}
 							</div>
 						</form>
 					</div>
